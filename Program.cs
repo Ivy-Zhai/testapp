b6d770d71c2c524f39752c5be0c8dd75c3dd57d5
Ivy-Zhai <55321356+Ivy-Zhai@users.noreply.github.com>
2019-09-15 15:22:01 +0800

    {0}相当于/d ?

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace testapp
                 Console.WriteLine("This is my first C# program~");
                 Console.Beep(1000,1000);
             }
+            Console.WriteLine("{0}",a);
         }
     }
 }
